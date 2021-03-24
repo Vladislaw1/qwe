0021429da1866b344897c8d678265bcecb43e2ec
Vladislaw1 <vladzajcenko077@gmail.com>
2021-03-24 15:30:42 +0200

    asdf

--- src/scripts/qwe.js
+++
@@ -1,8 +0,0 @@
-const btnCorzina = document.querySelector('.card__button');
-
-btnCorzina.addEventListener('click', onChangeText);
-
-export function onChangeText(e){
-    btnCorzina.textContent = "Добавлено в корзину";
-    btnCorzina.style.color = "green";
- }
